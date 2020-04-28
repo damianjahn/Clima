42f2ea0e22676f8611417ce64156ef6326b2e648
Damian <damian.jahn@icloud.com>
2020-04-28 15:53:29 +0200

    working with API

---
+++ Clima/WeatherManager.swift
@@ -0,0 +1,39 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Damian Jahn on 28/04/2020.
+//
+
+import Foundation
+
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=8e0aa477eb3aa89750d92bc9b0d7673d&units=metric"
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        if let url = URL(string: urlString) {
+            
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url, completionHandler: handle(data: response: error: ))
+            task.resume()
+        }
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
+    }
+}
